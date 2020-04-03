b43f94f9c069fc78503c8f50a900815557d2e517
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2020-04-03 12:35:13 +0200

    stamps

--- States/Assets/scripts/IState.cs
+++ States/Assets/scripts/IState.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿///////////////Attribution Notice://///////////////////////////////
+///This program was written by Armando Roerdinkveldboom, 2019/////
+///Contact at kazekage2002@gmail.com/////////////////////////////
+///Intended for educational use.////////////////////////////////
+///Rights reserved under Academic Free License v3.0////////////
+//////////////////////////////////////////////////////////////
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
