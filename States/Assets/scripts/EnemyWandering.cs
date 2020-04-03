d303245caa94a3bdc6f9bd091ec702b995894b4a
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2020-04-03 12:30:18 +0200

    upload

---
+++ States/Assets/scripts/EnemyWandering.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWandering : IState
+{
+    Rigidbody2D rb;
+    Vector3 patrolA;
+    Vector3 patrolB;
+    bool movinToA;
+    float speed;
+    public EnemyWandering(Rigidbody2D _rb,Vector3 A,Vector3 B,float _speed)
+    {
+        rb = _rb;
+        patrolA = A;
+        patrolB = B;
+        speed = _speed;
+    }
+    public void Enter()
+    {
+        movinToA = true;
+    }
+
+    public void Execute()
+    {
+        Vector2 pos=rb.transform.position;
+        if (movinToA)
+            pos = Vector2.MoveTowards(pos,patrolA,speed);
+        else
+        {
+            pos = Vector2.MoveTowards(pos, patrolB, speed);
+        }
+        rb.transform.position = pos;
+    }
+
+    public void Exit()
+    {
+        
+    }
+}
---
+++ States/Assets/scripts/StateMachine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine
+{
+    public IState currentState;
+    public IState prevState;
+    public void ChangeState(IState newState)
+    {
+        if (currentState != null)
+            currentState.Exit();
+        prevState = currentState;
+
+       
+            currentState = newState;
+            currentState.Enter();
+        
+           
+        
+    }
+
+    public void Update()
+    {
+        if (currentState != null) currentState.Execute();
+    }
+}
